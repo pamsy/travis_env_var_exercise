ed3e0bff85895ce1b69318098e2bf619d003ec62
pamsy <pamelaorline@gmail.com>
2022-09-23 17:14:02 +0100

    test

---
+++ server.js
@@ -0,0 +1,15 @@
+// Définir une variable locale alimentée par la variable d'environnement $NAME
+const FAVORITE_INSTRUCTOR = process.env.NAME;
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+  while(true) {
+    console.log("Microservices rock!");
+    await sleep(5000);
+  }
+}
+
+main();
